3e441ccdff0f9fe3fcf74f89ac37df503e8471b0
Dmytro Zaverukha <dmytrozaverukha@gmail.com>
2017-04-15 01:12:25 +0300

    Add packages

---
+++ OwnersPets/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Dmytro Zaverukha")]
+[assembly: AssemblyTrademark("™ Dmytro Zaverukha")]
+[assembly: AssemblyCopyright("© Dmytro Zaverukha. All right reserved.")]
+[assembly: AssemblyFileVersion("2.0.0")]
+
+// This shows up as Product Version in Windows Explorer
+// We make this the same for all files in a particular product version. And increment it globally for all projects.
+// We then use this as the Product Version in installers as well (for example built using Wix).
+[assembly: AssemblyInformationalVersion("2.0.0.0")]

321beb14a1ace06557b1c2acfb38939807e99011
Dmytro Zaverukha <dmytrozaverukha@gmail.com>
2017-04-15 00:46:56 +0300

    Add business models

---
+++ OwnersPets/Business/Models/OwnerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Business.Models
+{
+    using System.Collections.Generic;
+
+    public class OwnerModel
+    {
+        public string OwnerId { get; set; }
+
+        public string Name { get; set; }
+
+        public int PetsCount { get; set; }
+
+        public virtual ICollection<PetModel> Pets { get; set; } 
+    }
+}
---
+++ OwnersPets/Business/Models/PetModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Business.Models
+{
+    public class PetModel
+    {
+        public string PetId { get; set; }
+
+        public string Name { get; set; }
+    }
+}

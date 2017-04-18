9735ee60f43ef4cbccb989c687590b4b92e3b18c
Dmytro Zaverukha <dmytrozaverukha@gmail.com>
2017-04-18 23:39:13 +0300

    Update controllers

--- OwnersPets/OwnersPets/Controllers/OwnersController.cs
+++ OwnersPets/OwnersPets/Controllers/OwnersController.cs
@@ -3,35 +3,78 @@ namespace OwnersPets.WebApi.Controllers
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Business.Models;
+    using DataAccess.Entities;
+    using DataAccess.Repository;
     using Microsoft.AspNetCore.Mvc;
 
     [Produces("application/json")]
     [Route("api/Owners")]
     public class OwnersController : Controller
     {
+        private GlobalRepository globalRepository;
+
+        public OwnersController(GlobalRepository globalRepository)
+        {
+            this.globalRepository = globalRepository;
+        }
+
         // GET: api/Owners
         [HttpGet]
-        public async Task<IActionResult> GetOwners()
+        public async Task<IActionResult> GetOwners(int page, int itemsPerPage)
         {
-            return await Task.FromResult(this.Ok(new List<OwnerModel>()
+            List<OwnerModel> ownerModels = new List<OwnerModel>();
+
+            var ownersDb = await this.globalRepository.GetOwnersAsync(page, itemsPerPage);
+
+            if (ownersDb.Items.Count > 0)
             {
-                new OwnerModel { OwnerId = "ssds", Name = "John", PetsCount = 3 },
-                new OwnerModel { OwnerId = "sdwe", Name = "value2", PetsCount = 5 }
-            }));
+                foreach (var owner in ownersDb.Items)
+                {
+                    OwnerModel model = new OwnerModel
+                    {
+                        OwnerId = owner.OwnerId,
+                        Name = owner.Name,
+                        PetsCount = owner.PetsCount
+                    };
+
+                    ownerModels.Add(model);
+                }
+
+                return this.Ok(new { Owners = ownerModels, TotalItems = ownersDb.TotalCount });
+            }
+
+            return this.BadRequest("There are any owners");
         }
 
         // POST: api/Owners
         [HttpPost]
         public async Task<IActionResult> AddOwner([FromBody]OwnerModel ownerModel)
         {
-            return await Task.FromResult(this.Ok(true));
+            var ownerDb = (Owner)ownerModel;
+
+            var result = await this.globalRepository.AddOwnerAsync(ownerDb);
+
+            if (result)
+            {
+                return this.Ok(ownerModel);
+            }
+
+            return this.BadRequest("Error add owner");
         }
 
         // DELETE: api/Owners/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteOwner(string ownerId)
+        [HttpDelete]
+        [Route("DeleteOwner")]
+        public async Task<IActionResult> DeleteOwner([FromQuery]string ownerId)
         {
-            return await Task.FromResult(this.Ok(true));
+            var result = await this.globalRepository.DeleteOwnerAsync(ownerId);
+
+            if (result)
+            {
+                return this.Ok();
+            }
+
+            return this.BadRequest("Error delete owner");
         }
     }
 }
--- OwnersPets/OwnersPets/Controllers/PetsController.cs
+++ OwnersPets/OwnersPets/Controllers/PetsController.cs
@@ -3,35 +3,82 @@ namespace OwnersPets.WebApi.Controllers
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Business.Models;
+    using DataAccess.Entities;
+    using DataAccess.Repository;
     using Microsoft.AspNetCore.Mvc;
 
     [Produces("application/json")]
     [Route("api/Pets")]
     public class PetsController : Controller
     {
+        private GlobalRepository globalRepository;
+
+        public PetsController(GlobalRepository globalRepository)
+        {
+            this.globalRepository = globalRepository;
+        }
+
         // GET: api/Pets
         [HttpGet]
         public async Task<IActionResult> GetOwnerPets(string ownerId)
         {
-            return await Task.FromResult(this.Ok(new List<PetModel>()
+            var ownerWithPetsDb = await this.globalRepository.GetOwnerByIdAsync(ownerId);
+
+            if (ownerWithPetsDb != null)
             {
-                new PetModel { PetId = "sdcx", Name = "Dixi" },
-                new PetModel { PetId = "cvcv", Name = "value2" }
-            }));
+
+                OwnerModel ownerModel = new OwnerModel
+                {
+                    OwnerId = ownerWithPetsDb.OwnerId,
+                    Name = ownerWithPetsDb.Name,
+                    PetsCount = ownerWithPetsDb.PetsCount
+                };
+
+                foreach (var pet in ownerWithPetsDb.Pets)
+                {
+                    PetModel petModel = new PetModel
+                    {
+                        PetId = pet.PetId,
+                        Name = pet.Name
+                    };
+
+                    ownerModel.Pets.Add(petModel);
+                }
+
+                return this.Ok(ownerModel);
+            }
+
+            return this.BadRequest("Error retrieving owner's pets");
         }
 
         // POST: api/Pets
         [HttpPost]
         public async Task<IActionResult> AddOwnerPet([FromBody]PetModel petModel)
         {
-            return await Task.FromResult(this.Ok(true));
+            var petDb = (Pet)petModel;
+
+            var result = await this.globalRepository.AddOwnerPetAsync(petDb);
+
+            if (result)
+            {
+                return this.Ok();
+            }
+
+            return this.BadRequest("Error add owner's pet");
         }
 
         // DELETE: api/Pets/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteOwnerPet(string petId)
         {
-            return await Task.FromResult(this.Ok(true));
+            var result = await this.globalRepository.DeleteOwnerPetAsync(petId);
+
+            if (result)
+            {
+                return this.Ok();
+            }
+
+            return this.BadRequest("Error delete owner's pet");
         }
     }
 }
